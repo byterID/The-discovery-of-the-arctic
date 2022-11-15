37aeef78301d603251c57926c32c71e652384e52
byterID <artemijgladkov44644@gmail.com>
2022-11-15 18:48:24 +0500

    Tutorials Scenehierarchy Quest Done

---
+++ Assets/Scrips/TeleportTutorial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTutorial : MonoBehaviour
+{
+    public GameObject TutorialCanvas;
+    public GameObject TutorialTrigger;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            TutorialCanvas.SetActive(true);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            TutorialCanvas.SetActive(false);
+            TutorialTrigger.SetActive(false);
+        }
+    }
+}
